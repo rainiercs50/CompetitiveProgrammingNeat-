af7c43b7f6f2c19b0dba185d43ffd3ec66d9d279
Rainier Hardjanto <65476882+rainiercs50@users.noreply.github.com>
2021-06-23 23:02:40 +0700

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,23 +1,31 @@
-int p[mxN], s[mxN], n;
+struct DSU{
+	vector<int> p, s;
+	void init(int n){
+		p.resize(n);
+		s.resize(n);
+		iota(p.begin(), p.end(), 0);
+		fill(s.begin(), s.end(), 1);
+	}
+	int find(int x){
+		return x^p[x]?p[x]=find(p[x]):x;
+	}
+	bool join(int a, int b){
+		int x=find(a), y=find(b);
+		if(x==y) return 0;
+		if(s[x]<s[y]) swap(x,y);
+		p[y]=x;
+		s[x]+=s[y];
+		return 1;
+	}
+	
+};
 
-int find(int x){
-	return x^p[x]?p[x]=find(p[x]):x;
-}
-bool join(int a, int b){
-	int x=find(a), y=find(b);
-	if(x==y) return 0;
-	if(s[x]<s[y]) swap(x,y);
-	p[y]=x;
-	s[x]+=s[y];
-	return 1;
-}
-void init(){
-	iota(p, p+n ,0);
-	fill(s, s+n, 1);
-}
-ll kruskal(vector<pair<ll, pair<int,int>>> ed){
+
+ll kruskal(int n, vector<pair<ll, pair<int,int>>> ed){
 	sort(ed.begin(), ed.end());
 	ll ans=0;
-	for(auto a:ed) if(join(a.second.second,a.second.first)) ans+=a.first;
+	DSU d;
+	d.init(n);
+	for(auto a:ed) if(d.join(a.second.second,a.second.first)) ans+=a.first;
 	return ans;
 }
