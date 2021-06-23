d0886a5f47d44b42cd6c41ba0cb2d4a02a56192e
Rainier Hardjanto <65476882+rainiercs50@users.noreply.github.com>
2021-06-23 13:15:40 +0700

    Create StandardTemplate.cpp

---
+++ StandardTemplate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ar array
+ 
+const int mxN=1e5;
+
+
+void solve(int tc){
+	
+}
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	cin >> t;
+	for(int tc=0; tc<t; ++tc) solve(tc);
+}
