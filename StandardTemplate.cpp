ce0deb6b456a5afea7a2e17355e164da8be934f2
Rainier Hardjanto <65476882+rainiercs50@users.noreply.github.com>
2021-07-05 10:59:08 +0700

    Update StandardTemplate.cpp

--- StandardTemplate.cpp
+++ StandardTemplate.cpp
@@ -5,14 +5,12 @@
 using namespace std;
 using ll = long long;
 
-void solve(int tc){
-	
-
-}
-
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(15);
 
-	int T = 1; cin >> T;
+	int T = 1; // cin >> T;
+	auto solve = [&](int tc){
+		
+	};
 	for(int tc = 0; tc < T; ++tc) solve(tc);
 }
