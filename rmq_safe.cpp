da25abd8dc6cbd65449a15ecdd16c27f5aa7b060
Rainier Hardjanto <65476882+rainiercs50@users.noreply.github.com>
2021-08-02 08:49:21 +0700

    Update rmq_safe.cpp

--- rmq_safe.cpp
+++ rmq_safe.cpp
@@ -1,36 +1,17 @@
-template<class T> struct RMQmin { // floor(log_2(x))
-	int level(int x) { return 31-__builtin_clz(x); } 
-	vector<T> v; vector<vector<int>> jmp;
-	int comb(int a, int b) { // index of min
-		return v[a]==v[b]?min(a,b):(v[a]<v[b]?a:b); } 
-	void init(const vector<T>& _v) {
-		v = _v; jmp = {vector<int>(sz(v))}; iota(all(jmp[0]),0);
-		for (int j = 1; 1<<j <= sz(v); ++j) {
-			jmp.push_back(vector<int>(sz(v)-(1<<j)+1));
-			for(int i=0; i<sz(jmp[j]); ++i) jmp[j][i] = comb(jmp[j-1][i],
-									jmp[j-1][i+(1<<(j-1))]);
+template<class T>
+struct RMQ {
+	#define rep(i, a, b) for(int i = a; i < (b); ++i)
+	vector<vector<T>> jmp;
+	RMQ(const vector<T>& V) : jmp(1, V) {
+		for (int pw = 1, k = 1; pw * 2 <= sz(V); pw *= 2, ++k) {
+			jmp.emplace_back(sz(V) - pw * 2 + 1);
+			rep(j,0,sz(jmp[k]))
+				jmp[k][j] = __gcd(jmp[k - 1][j], jmp[k - 1][j + pw]);
 		}
 	}
-	int index(int l, int r) { // get index of min element
-		assert(l <= r); int d = level(r-l+1);
-		return comb(jmp[d][l],jmp[d][r-(1<<d)+1]); }
-	T query(int l, int r) { return v[index(l,r)]; }
-};
-template<class T> struct RMQmax { // floor(log_2(x))
-	int level(int x) { return 31-__builtin_clz(x); } 
-	vector<T> v; vector<vector<int>> jmp;
-	int comb(int a, int b) { // index of max 
-		return v[a]==v[b]?min(a,b):(v[a]>v[b]?a:b); } 
-	void init(const vector<T>& _v) {
-		v = _v; jmp = {vector<int>(sz(v))}; iota(all(jmp[0]),0);
-		for (int j = 1; 1<<j <= sz(v); ++j) {
-			jmp.push_back(vector<int>(sz(v)-(1<<j)+1));
-			for(int i=0; i<sz(jmp[j]); ++i) jmp[j][i] = comb(jmp[j-1][i],
-									jmp[j-1][i+(1<<(j-1))]);
-		}
+	T query(int a, int b) {
+        b++;
+		int dep = 31 - __builtin_clz(b - a);
+		return __gcd(jmp[dep][a], jmp[dep][b - (1 << dep)]);
 	}
-	int index(int l, int r) { // get index of max element
-		assert(l <= r); int d = level(r-l+1);
-		return comb(jmp[d][l],jmp[d][r-(1<<d)+1]); }
-	T query(int l, int r) { return v[index(l,r)]; }
 };
