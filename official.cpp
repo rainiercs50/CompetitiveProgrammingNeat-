7ccf479ed21a24ed85eaea8b98b8a12fe6d1b617
Rainier Hardjanto <65476882+rainiercs50@users.noreply.github.com>
2021-12-02 21:31:05 +0700

    Create official.cpp

---
+++ official.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define int long long 
+
+
+void solve(){
+   
+}
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+
+    
+}
